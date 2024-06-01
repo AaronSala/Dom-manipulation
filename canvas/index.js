d56703976b2de94aadeea728ac0590fce49280e1
Aaron Sala <salaaron2@gmail.com>
2024-06-01 18:58:36 +0300

    randomising the circles created

--- canvas/index.js
+++ canvas/index.js
@@ -32,33 +32,60 @@ const c = canvas.getContext("2d");
 // c.strokeStyle = "#f134ac";
 // c.stroke();
 
-function Circle(){
-  
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = () => {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+  };
+
+  this.update = function () {
+      if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+       this.dx = -this.dx;
+      }
+      if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+        this.dy = -this.dy;
+      }
+      this.x += this.dx;
+    this.y += this.dy;
+    
+    this.draw();
+  }
 }
 
-let x = Math.random()*innerWidth;
-let dx = (Math.random()-0.5)*8
-let y = Math.random()*innerHeight
-let dy=(Math.random()-0.5)*8
-let radius=30
+
+let circleArray = []
+
+
+for (let i = 0; i < 100; i++) {
+
+  let radius = 30;
+let x = Math.random() * (innerWidth - radius*2)+radius;
+let y = Math.random() * (innerHeight - radius*2)+radius;
+let dx = (Math.random() - 0.5) ;
+
+let dy = (Math.random() - 0.5);
+
+  circleArray.push(new Circle(x, y, dx, dy, radius))
+
+}
+
+console.log(circleArray)
 
 function animate() {
-  requestAnimationFrame(animate)
-c.clearRect(0, 0, innerWidth, innerHeight)
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  
+  for (let i = 0; i < circleArray.length; i++){
+    circleArray[i].update()
+  }
 
-c.beginPath();
-c.arc(x, y, 40, 0, Math.PI * 2, false);
-c.strokeStyle = "blue";
-  c.stroke();
-  
-  if (x + radius> innerWidth || x-radius<0) { 
-    dx=-dx
-  }
-  if (y + radius > innerHeight || y-radius<0) {
-    dy= -dy
-  }
-  x += dx
-  y +=dy
-  
 }
-animate()
+animate();
