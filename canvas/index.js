cb090975aceba37c0d344ee552680e6a19526bd4
Aaron Sala <salaaron2@gmail.com>
2024-06-02 05:53:16 +0300

    canvas with random circles and mouse move event

--- canvas/index.js
+++ canvas/index.js
@@ -31,6 +31,25 @@ const c = canvas.getContext("2d");
 // c.lineTo(150, 300);
 // c.strokeStyle = "#f134ac";
 // c.stroke();
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+let maxRadius = 40;
+//let minRadius = 8;
+
+let colorArray = ["#005990", "#E69004", "#E7B744", "#DAD9D9", "#A83B0D"];
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+});
 
 function Circle(x, y, dx, dy, radius) {
   this.x = x;
@@ -38,54 +57,69 @@ function Circle(x, y, dx, dy, radius) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+  this.minRadius = radius;
 
   this.draw = () => {
     c.beginPath();
+    c.fillStyle = this.color;
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.strokeStyle = "blue";
+
+    c.fill();
     c.stroke();
   };
 
   this.update = function () {
-      if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-       this.dx = -this.dx;
-      }
-      if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-        this.dy = -this.dy;
-      }
-      this.x += this.dx;
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
     this.y += this.dy;
-    
+
+    //interactivity
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > this.minRadius) {
+      this.radius -= 1;
+    }
     this.draw();
+  };
+}
+
+let circleArray = [];
+
+function init() {
+  circleArray = [];
+
+  for (let i = 0; i < 1000; i++) {
+    let radius = Math.random() * 3 + 1;
+    let x = Math.random() * (innerWidth - radius * 2) + radius;
+    let y = Math.random() * (innerHeight - radius * 2) + radius;
+    let dx = Math.random() - 0.5;
+
+    let dy = Math.random() - 0.5;
+
+    circleArray.push(new Circle(x, y, dx, dy, radius));
   }
 }
-
-
-let circleArray = []
-
-
-for (let i = 0; i < 100; i++) {
-
-  let radius = 30;
-let x = Math.random() * (innerWidth - radius*2)+radius;
-let y = Math.random() * (innerHeight - radius*2)+radius;
-let dx = (Math.random() - 0.5) ;
-
-let dy = (Math.random() - 0.5);
-
-  circleArray.push(new Circle(x, y, dx, dy, radius))
-
-}
-
-console.log(circleArray)
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
-  
-  for (let i = 0; i < circleArray.length; i++){
-    circleArray[i].update()
-  }
 
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
 }
+init();
 animate();
